5f33fc64229052b02018e188daa9d0990a1baf8a
kvsa01 <168811748+kvsa01@users.noreply.github.com>
2024-05-03 04:02:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,22 +18,22 @@ fig, ax = plt.subplots(1, 3, figsize=(15, 5))
 # Plotting the area plot
 ax[0].fill_between(x, resnet50, color='blue', alpha=0.3, label='ResNet50')
 ax[0].fill_between(x, alexnet, color='red', alpha=0.3, label='AlexNet')
-ax[0].fill_between(x, inception3, color='green', alpha=0.3, label='Inception3')
+ax[0].fill_between(x, inception3, color='green', alpha=0.3, label='InceptionV3')
 ax[0].set_title('Area Plot')
 ax[0].legend()
-plt.title('Comparision b/w ResNet50 vs AlexNet vs Inception V3')
+plt.title('Comparision b/w ResNet50 vs AlexNet vs InceptionV3')
 
 # Plotting the line graph
 ax[1].plot(x, resnet50, color='blue', label='ResNet50')
 ax[1].plot(x, alexnet, color='red', label='AlexNet')
-ax[1].plot(x, inception3, color='green', label='Inception3')
+ax[1].plot(x, inception3, color='green', label='InceptionV3')
 ax[1].set_title('Line Graph')
 ax[1].legend()
 
 # Plotting the bar chart
 ax[2].bar(x, resnet50, color='blue', alpha=0.5, label='ResNet50')
 ax[2].bar(x, alexnet, color='red', alpha=0.5, label='AlexNet')
-ax[2].bar(x, inception3, color='green', alpha=0.5, label='Inception3')
+ax[2].bar(x, inception3, color='green', alpha=0.5, label='InceptionV3')
 ax[2].set_title('Bar Chart')
 ax[2].legend()
 
